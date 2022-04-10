2bc63198e590b8a6974eb18e159625d82190be46
Dongd_Zhou <640615419@qq.com>
2022-04-10 15:57:22 +0800

    import

--- common/src/main/java/cn/zm1001/util/common/HttpUtils.java
+++ common/src/main/java/cn/zm1001/util/common/HttpUtils.java
@@ -4,11 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
 
 import javax.servlet.ServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
