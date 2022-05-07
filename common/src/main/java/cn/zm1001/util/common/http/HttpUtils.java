bb46978bc4bff651e6c92c9d28954912585ac880
Dongd_Zhou <dongdong.zhou@xdxct.com>
2022-05-07 14:24:24 +0800

    表单提交及上传

--- common/src/main/java/cn/zm1001/util/common/http/HttpUtils.java
+++ common/src/main/java/cn/zm1001/util/common/http/HttpUtils.java
@@ -5,19 +5,29 @@ import cn.zm1001.util.common.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpMessage;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * @Desc HTTP工具类
@@ -87,7 +97,7 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#get# #exception# #{}# {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), e);
         } finally {
-            log.info("#get# #rsp# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), content);
+            log.info("#get# #result# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), content);
         }
         return content;
     }
@@ -117,7 +127,53 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#postXml# #exception# #{}# {} {}", getUrl(url), url, xml, e);
         } finally {
-            log.info("#postXml# #rsp# #{}# {} {} {}", getUrl(url), url, xml, content);
+            log.info("#postXml# #result# #{}# {} {} {}", getUrl(url), url, xml, content);
+        }
+        return content;
+    }
+
+    /**
+     * 表单提交
+     *
+     * @param url      请求地址
+     * @param paramMap 参数
+     * @return 响应消息
+     */
+    public static String doPostForm(String url, Map<String, String> paramMap) {
+        return doPostFormWithHeader(url, null, paramMap);
+    }
+
+    /**
+     * 表单提交
+     *
+     * @param url       请求地址
+     * @param headerMap 头部信息
+     * @param paramMap  参数
+     * @return 响应消息
+     */
+    public static String doPostFormWithHeader(String url, Map<String, String> headerMap, Map<String, String> paramMap) {
+        String content = null;
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(url);
+            // 请求头部
+            setHeader(httpPost, headerMap);
+            // 请求体
+            List<NameValuePair> formParams = new ArrayList<>();
+            for (Entry<String, String> entry : paramMap.entrySet()) {
+                formParams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+            httpPost.setEntity(new UrlEncodedFormEntity(formParams, Consts.UTF_8));
+
+            CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+
+            // 返回200
+            if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                content = EntityUtils.toString(httpResponse.getEntity(), Consts.UTF_8);
+            }
+        } catch (Exception e) {
+            log.error("#post# #exception# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), e);
+        } finally {
+            log.info("#post# #result# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), content);
         }
         return content;
     }
@@ -136,8 +192,9 @@ public class HttpUtils {
     /**
      * POST请求，请求体转为JSON数据
      *
-     * @param url      请求地址
-     * @param paramMap 参数
+     * @param url       请求地址
+     * @param headerMap 头部信息
+     * @param paramMap  参数
      * @return 响应消息
      */
     public static String doPostWithHeader(String url, Map<String, String> headerMap, Map<String, Object> paramMap) {
@@ -161,7 +218,7 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#post# #exception# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), e);
         } finally {
-            log.info("#post# #rsp# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), content);
+            log.info("#post# #result# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), content);
         }
         return content;
     }
@@ -204,7 +261,7 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#post# #exception# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, e);
         } finally {
-            log.info("#post# #rsp# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, content);
+            log.info("#post# #result# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, content);
         }
         return content;
     }
@@ -250,7 +307,7 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#put# #exception# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), e);
         } finally {
-            log.info("#put# #rsp# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), content);
+            log.info("#put# #result# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), content);
         }
         return content;
     }
@@ -295,7 +352,7 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#put# #exception# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, e);
         } finally {
-            log.info("#put# #rsp# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, content);
+            log.info("#put# #result# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, content);
         }
         return content;
     }
@@ -334,7 +391,7 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#delete# #exception# #{}# {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), e);
         } finally {
-            log.info("#delete# #rsp# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), content);
+            log.info("#delete# #result# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), content);
         }
         return content;
     }
@@ -380,7 +437,7 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#delete# #exception# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), e);
         } finally {
-            log.info("#delete# #rsp# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), content);
+            log.info("#delete# #result# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), JacksonUtils.toJson(paramMap), content);
         }
         return content;
     }
@@ -425,9 +482,110 @@ public class HttpUtils {
         } catch (Exception e) {
             log.error("#delete# #exception# #{}# {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, e);
         } finally {
-            log.info("#delete# #rsp# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, content);
+            log.info("#delete# #result# #{}# {} {} {} {}", getUrl(url), url, JacksonUtils.toJson(headerMap), json, content);
         }
         return content;
     }
 
+    /**
+     * 上传
+     *
+     * @param url  请求地址
+     * @param file 上传文件
+     * @return 响应消息
+     */
+    public static String upload(String url, File file) {
+        return upload(url, file, null);
+    }
+
+    /**
+     * 上传
+     *
+     * @param url      请求地址
+     * @param file     上传文件
+     * @param paramMap 参数
+     * @return 响应消息
+     */
+    public static String upload(String url, File file, Map<String, String> paramMap) {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody("file", file);
+        if (MapUtils.isNotEmpty(paramMap)) {
+            for (Entry<String, String> entry : paramMap.entrySet()) {
+                builder.addTextBody(entry.getKey(), entry.getValue());
+            }
+        }
+        final HttpEntity dataEntity = builder.build();
+        return upload(url, null, dataEntity);
+    }
+
+    /**
+     * 上传
+     *
+     * @param url      请求地址
+     * @param bytes    图片字节码
+     * @param paramMap 参数
+     * @return 响应消息
+     */
+    public static String upload(String url, byte[] bytes, Map<String, String> paramMap) {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody("file", bytes);
+        if (MapUtils.isNotEmpty(paramMap)) {
+            for (Entry<String, String> entry : paramMap.entrySet()) {
+                builder.addTextBody(entry.getKey(), entry.getValue());
+            }
+        }
+        final HttpEntity dataEntity = builder.build();
+        return upload(url, null, dataEntity);
+    }
+
+    /**
+     * 上传
+     *
+     * @param url      请求地址
+     * @param is       图片流
+     * @param paramMap 参数
+     * @return 响应消息
+     */
+    public static String upload(String url, InputStream is, Map<String, String> paramMap) {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody("file", is);
+        if (MapUtils.isNotEmpty(paramMap)) {
+            for (Entry<String, String> entry : paramMap.entrySet()) {
+                builder.addTextBody(entry.getKey(), entry.getValue());
+            }
+        }
+        final HttpEntity dataEntity = builder.build();
+        return upload(url, null, dataEntity);
+    }
+
+    /**
+     * 上传
+     *
+     * @param url        请求地址
+     * @param headerMap  头部
+     * @param dataEntity 上传信息
+     * @return 响应消息
+     */
+    public static String upload(String url, Map<String, String> headerMap, HttpEntity dataEntity) {
+        String content = null;
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(url);
+            // 请求头部
+            setHeader(httpPost, headerMap);
+            // 请求体
+            httpPost.setEntity(dataEntity);
+
+            CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+
+            // 返回200
+            if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                content = EntityUtils.toString(httpResponse.getEntity(), Consts.UTF_8);
+            }
+        } catch (Exception e) {
+            log.error("#upload# #exception# #{}# {} ", getUrl(url), url, e);
+        } finally {
+            log.info("#upload# #result# #{}# {} {}", getUrl(url), url, content);
+        }
+        return content;
+    }
 }
