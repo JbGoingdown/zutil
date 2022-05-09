d45e3ae2f1e03791d47e0874221b0310ab9a28d4
Dongd_Zhou <dongdong.zhou@xdxct.com>
2022-05-09 11:55:09 +0800

    本地IP及Mac

--- common/src/main/java/cn/zm1001/util/common/WebUtils.java
+++ common/src/main/java/cn/zm1001/util/common/WebUtils.java
@@ -3,6 +3,7 @@ package cn.zm1001.util.common;
 import cn.zm1001.util.common.http.HttpUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.Validate;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +11,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * @Desc 网路地址工具类
@@ -27,6 +37,154 @@ public class WebUtils {
     public static final String UNKNOWN = "unknown";
     public static final String DEFAULT_IPV6 = "0:0:0:0:0:0:0:1";
     public static final String DEFAULT_IPV4 = "127.0.0.1";
+
+    /**
+     * 本机地址
+     *
+     * @return 地址列表
+     */
+    public static List<String> getHostIP() {
+        List<String> ips = new ArrayList<>();
+        try {
+            final Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface nif;
+            Enumeration<InetAddress> inetAddresses;
+            InetAddress ipAddress;
+            while (en.hasMoreElements()) {
+                nif = en.nextElement();
+                inetAddresses = nif.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    ipAddress = inetAddresses.nextElement();
+                    if (!ipAddress.isLoopbackAddress() && !ipAddress.isLinkLocalAddress() && ipAddress.isSiteLocalAddress()) {
+                        ips.add(ipAddress.getHostAddress());
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            log.error("#getHostIP# ## ## 获取本机地址失败", e);
+        }
+        return ips;
+    }
+
+    /**
+     * 本机指定网卡地址
+     *
+     * @param networkCard 网卡名称
+     * @return 地址
+     */
+    public static String getHostIPByNetworkCard(String networkCard) {
+        Validate.notEmpty(networkCard, "network card name is required");
+        String ip = null;
+        try {
+            final Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface nif;
+            Enumeration<InetAddress> inetAddresses;
+            InetAddress ipAddress;
+            while (en.hasMoreElements()) {
+                nif = en.nextElement();
+                if (nif.getName().equals(networkCard)) {
+                    inetAddresses = nif.getInetAddresses();
+                    while (inetAddresses.hasMoreElements()) {
+                        ipAddress = inetAddresses.nextElement();
+                        if (ipAddress instanceof Inet6Address) {
+                            // 忽略IPv6地址
+                            continue;
+                        }
+                        ip = ipAddress.getHostAddress();
+                    }
+                    break;
+                }
+            }
+        } catch (SocketException e) {
+            log.error("#getHostIPByNetworkCard# #{}# ## 获取本机指定网卡地址失败", networkCard, e);
+        }
+        return ip;
+    }
+
+    /**
+     * 本机MAC地址
+     *
+     * @return MAC地址列表
+     */
+    public static Set<String> getHostMac() {
+        Set<String> macs = new HashSet<>();
+        try {
+            StringBuilder sb = new StringBuilder();
+            final Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface nif;
+            Enumeration<InetAddress> inetAddresses;
+            InetAddress ipAddress;
+            NetworkInterface network;
+            while (en.hasMoreElements()) {
+                nif = en.nextElement();
+                inetAddresses = nif.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    ipAddress = inetAddresses.nextElement();
+                    network = NetworkInterface.getByInetAddress(ipAddress);
+                    if (null == network) {
+                        continue;
+                    }
+                    byte[] mac = network.getHardwareAddress();
+                    if (null == mac) {
+                        continue;
+                    }
+                    sb.delete(0, sb.length());
+                    for (int i = 0, len = mac.length; i < len; i++) {
+                        sb.append(String.format("%02X%s", mac[i], (i < len - 1) ? "-" : ""));
+                    }
+                    macs.add(sb.toString());
+                }
+            }
+        } catch (SocketException e) {
+            log.error("#getHostMac# ## ## 获取本机MAC地址失败", e);
+        }
+        return macs;
+    }
+
+    /**
+     * 本机指定网卡MAC地址
+     *
+     * @return MAC地址
+     */
+    public static String getHostMacByNetworkCard(String networkCard) {
+        Validate.notEmpty(networkCard, "network card name is required");
+        String macAddress = null;
+        try {
+            StringBuilder sb = new StringBuilder();
+            final Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface nif;
+            Enumeration<InetAddress> inetAddresses;
+            InetAddress ipAddress;
+            NetworkInterface network;
+            while (en.hasMoreElements()) {
+                nif = en.nextElement();
+                if (nif.getName().equals(networkCard)) {
+                    inetAddresses = nif.getInetAddresses();
+                    while (inetAddresses.hasMoreElements()) {
+                        ipAddress = inetAddresses.nextElement();
+                        network = NetworkInterface.getByInetAddress(ipAddress);
+                        if (null == network) {
+                            continue;
+                        }
+                        byte[] mac = network.getHardwareAddress();
+                        if (null == mac) {
+                            continue;
+                        }
+                        sb.delete(0, sb.length());
+                        for (int i = 0, len = mac.length; i < len; i++) {
+                            sb.append(String.format("%02X%s", mac[i], (i < len - 1) ? "-" : ""));
+                        }
+                        macAddress = sb.toString();
+                    }
+                    break;
+                }
+
+            }
+        } catch (SocketException e) {
+            log.error("#getHostMacByNetworkCard# #{}# ## 获取本机指定网卡MAC地址失败", networkCard, e);
+        }
+        return macAddress;
+    }
 
     /**
      * 通过IP地址获取真实地址
@@ -84,6 +242,12 @@ public class WebUtils {
         return DEFAULT_IPV6.equals(ip) ? DEFAULT_IPV4 : ip;
     }
 
+    /**
+     * 是否内网地址
+     *
+     * @param ip IP地址
+     * @return 是否内网地址
+     */
     public static boolean internalIp(String ip) {
         byte[] addr = textToNumericFormatV4(ip);
         return internalIp(addr) || DEFAULT_IPV4.equals(ip);
