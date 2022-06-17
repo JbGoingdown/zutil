dcddcbe5be2f635e48eb41a56ee0416cf0819e72
Dongd_Zhou <640615419@qq.com>
2022-06-18 00:03:20 +0800

    注释

--- common/src/main/java/cn/zm1001/util/common/xml/AdapterCDATA.java
+++ common/src/main/java/cn/zm1001/util/common/xml/AdapterCDATA.java
@@ -5,6 +5,11 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 /**
  * @author Dongd_Zhou
  * @desc Xml数据的序列化与反序列化
+ * 在属性上添加注解
+ * <pre>
+ * @XmlJavaTypeAdapter(AdapterCDATA.class)
+ * private String fieldName;
+ * </pre>
  */
 public class AdapterCDATA extends XmlAdapter<String, String> {
     @Override
